64507540968888889868829dec4898d3d5f404f5
Eden Tewelde <eden.tewelde@th-bingen.de>
2017-07-24 18:25:31 +0200

    TODO #3

--- src/main/kotlin/org/codefx/demo/bingen/bank/Main.kt
+++ src/main/kotlin/org/codefx/demo/bingen/bank/Main.kt
@@ -11,11 +11,15 @@ fun main(args : Array<String>) {
     val eden = bestbank.newCustomer("Eden")
     val john = bestbank.newCustomer("John")
     val account = bestbank.openAccount(eden,Money(50),Balance(-100))
+    val account2 = bestbank.openAccount(eden,Money(500),Balance(0))
     account.deposit(Money(100))
     account.withdraw(Money(300))
     val accj = bestbank.openAccount(john,Money(100),limit = Balance(-100))
     println(bestbank.withdraw(accj,Money(150)))
-    println(bestbank.closeAccount(john,accj))
+    println(bestbank.transferBetweenAccounts(account2,account,Money(100)))
+    println(bestbank.transferBetweenCustomers(eden,john,Money(30)))
+    println(bestbank.closeAccount(eden,account2))
+
 
 
 }
